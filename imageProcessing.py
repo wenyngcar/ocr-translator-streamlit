22fd6628d642898d421e06d8bbbb2f2e4c3f8c03
wenyngcar <owenyoungcarino@gmail.com>
2025-05-19 13:20:18 +0800

    feat: preprocessed and extract text from image

---
+++ imageProcessing.py
@@ -0,0 +1,26 @@
+import cv2
+from matplotlib import pyplot as plt
+image_file = "./images/tanga.jpg"
+img = cv2.imread(image_file)
+
+
+def display(im_path):
+    dpi = 80
+    im_data = plt.imread(im_path)
+
+    height, width = im_data.shape[:2]
+
+    # What size does the figure need to be in inches to fit the image?
+    figsize = width / float(dpi), height / float(dpi)
+
+    # Create a figure of the right size with one axes that takes up the full figure
+    fig = plt.figure(figsize=figsize)
+    ax = fig.add_axes([0, 0, 1, 1])
+
+    # Hide spines, ticks, etc.
+    ax.axis('off')
+
+    # Display the image.
+    ax.imshow(im_data, cmap='gray')
+
+    plt.show()
