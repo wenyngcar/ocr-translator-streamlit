cea560f29caa76e2b1c6af2440b2c18cadeb85e6
wenyngcar <owenyoungcarino@gmail.com>
2025-05-20 10:57:55 +0800

    refactor: adjust kernel for noise removal

--- imageProcessing.py
+++ imageProcessing.py
@@ -7,9 +7,9 @@ def grayscale(image):
 
 
 def noise_removal(image):
-    import numpy as np
     kernel = np.ones((1, 1), np.uint8)
     image = cv2.dilate(image, kernel, iterations=1)
+    image = cv2.erode(image, kernel, iterations=1)
     image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
     image = cv2.medianBlur(image, 3)
     return (image)
